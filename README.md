fcf3fc0ba6d2a3623c0939d9266f1c45c660fcb5
kasterra <kasterra@naver.com>
2021-04-28 11:13:25 +0000

    made to-do stuff editable.

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Clones momentum. And some useful information.
     - [ ] Make time format selectable
 - [X] Ask username and Reflect it
     - [ ] Make username editable
-- [ ] Random Background images
+- [X] Random Background images
 - [ ] Manage TodoList
     - [X] Create Todo
-    - [ ] Update Todo
+    - [X] Update Todo
     - [X] Delete Todo
 	- [ ] Mark as finished
 - [X] Save those personal stuffs in browser
 - ~~[ ] Show COVID-19 status~~ → lack of backend knowledge. I can't hide api keys.
-- [ ] Get Awesome Animation with CSS
+- [X] Get Awesome Animation with CSS
--- getTodo.js
+++ getTodo.js
@@ -6,13 +6,17 @@ const TODO_LS = 'todo';
 let toDos = [];
 
 function saveToDos() {
+	for(liNum = 0; liNum < toDos.length; liNum++){
+		toDos[liNum].id = liNum;
+	}
     localStorage.setItem(TODO_LS, JSON.stringify(toDos));
 }
 
 function deleteTodo(event) {
     const target = event.target.parentNode;
     const targetID = target.id;
-    todoList.removeChild(target);
+	target.style.opacity = '0';
+	setTimeout(function(){todoList.removeChild(target);},100);
     const filtered = toDos.filter(function(todo) {
         return todo.id+'' !== targetID;
     });
@@ -20,17 +24,46 @@ function deleteTodo(event) {
     saveToDos();
 }
 
+function editTodo(event){
+	event.preventDefault();
+	const target = event.target.parentNode;
+	const isEdit = target.classList.contains("editMode");
+	const text = isEdit ?target.querySelector('form').querySelector('input').value:target.querySelector('span').innerText;
+	if(isEdit){
+		toDos[target.id].text = text;
+		target.querySelector('span').innerText = text;
+		saveToDos();
+	}
+	else{
+		console.log(target);
+		target.querySelector('form').querySelector('input').value = text;
+	}
+	
+	target.classList.toggle("editMode");
+}
+
 function paintTodo(text, isFinished = false) {
     let li = document.createElement('li');
     let span = document.createElement('span');
-    span.innerText = text;
-    let icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-minus-circle');
-    icon.addEventListener('click', deleteTodo);
-    li.append(icon);
+    let deleteIcon = document.createElement('i');
+	let editIcon = document.createElement('i');
+	let form = document.createElement('form');
+	let input = document.createElement('input');
+	
+	span.innerText = text;
+    deleteIcon.classList.add('fas', 'fa-minus-circle');
+    deleteIcon.addEventListener('click', deleteTodo);
+	editIcon.classList.add('fas','fa-edit');
+	editIcon.addEventListener('click',editTodo);
+	form.addEventListener('submit',editTodo);
+    form.append(input);
+	li.append(deleteIcon);
+	li.append(editIcon);
     li.append(span);
-    li.id = liNum++;
-    todoList.append(li);
+	li.append(form);
+	li.id = liNum++;
+    
+	setTimeout(function(){todoList.append(li);},100);
     const todoObj = {
         text: span.innerText,
         id: li.id,
